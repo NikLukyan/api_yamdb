f6c1a3523141b6b03d0005295d4b52b17ac0d002
Дмитрий Бессмертный <ya@bessmerti.ru>
2023-01-17 18:24:55 +0300

    17.01 + users

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = 
+    email =
+    first_name = 
+    last_namebio =
+    role =
+
+    pass
+
