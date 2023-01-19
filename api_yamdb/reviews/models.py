438e8b9fe307c4b2fe9be84b8a96e0596b931e3e
NikLukyan <113015696+NikLukyan@users.noreply.github.com>
2023-01-19 22:41:35 +0300

    Comment model created (#4)
    * Create app and model Reviews
    
    * Create app and model Reviews
    
    * Create app and model Reviews, add score choices
    
    * Update Review model
    
    * Create Comment model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+
+
 # from api_yamdb.api.models import Title
+# from api_yamdb.user.models import User
+
+
+
 
 
 class Reviews(models.Model):
@@ -15,3 +21,14 @@ class Reviews(models.Model):
     score = models.PositiveSmallIntegerField(choices=SCORE_CHOICES)
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    # author = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
