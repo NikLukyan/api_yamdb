70dcdb43cae88cb92ce131fc1196d84a6dcfd7d3
NikLukyan <nikluk@mail.ru>
2023-01-19 15:54:32 +0300

    Create Comment model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 # from api_yamdb.api.models import Title
+# from api_yamdb.user.models import User
 
 
 class Review(models.Model):
@@ -15,5 +16,15 @@ class Review(models.Model):
     #     Title, on_delete=models.CASCADE, related_name='reviews')
     text = models.TextField()
     score = models.PositiveSmallIntegerField(choices=SCORE_CHOICES)
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    # author = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
