9b700386f091815f956dc2fc5967bbd18132bb48
NikLukyan <113015696+NikLukyan@users.noreply.github.com>
2023-01-20 20:26:04 +0300

    Feature/review (#7)
    * Update README, requirements, create __init__.py
    
    * Create API for reviews
    
    * Create API for reviews
    
    * Create API for reviews

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,16 +2,24 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from api.views import (TitlesViewSet, GenresViewSet, CategoryViewSet)
+from api.views import (TitlesViewSet,
+                       GenresViewSet,
+                       CategoryViewSet,
+                       ReviewViewSet)
+
+app_name = 'api'
 
 router = DefaultRouter()
 
 router.register('titles', TitlesViewSet, basename='titles')
 router.register('genres', GenresViewSet, basename='genres')
 router.register('categories', CategoryViewSet, basename='categories')
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet,
+                basename='review')
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router.urls), name='api_v1'),
     path('v1/auth/token', include('djoser.urls.jwt')),
 ]
--- api_yamdb/reviews/apps.py
+++ api_yamdb/reviews/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
+    verbose_name = 'отзывы'
