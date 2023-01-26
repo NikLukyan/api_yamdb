d0b45836ee87c09d97f9428ea9cb3847147c9c6c
NikLukyan <nikluk@mail.ru>
2023-01-26 23:15:36 +0300

    bugfix pep8

--- api_yamdb/api/v1/filters.py
+++ api_yamdb/api/v1/filters.py
@@ -6,7 +6,8 @@ from reviews.models import Title
 class TitleFilter(django_filters.FilterSet):
     genre = django_filters.CharFilter(field_name='genre__slug')
     category = django_filters.CharFilter(field_name='category__slug')
-    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    name = django_filters.CharFilter(field_name='name',
+                                     lookup_expr='icontains')
 
     class Meta:
         model = Title
