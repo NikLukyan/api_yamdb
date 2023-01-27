dac81a9ab7e65da1dc488a5d64fce592a8a9056e
NikLukyan <113015696+NikLukyan@users.noreply.github.com>
2023-01-27 23:10:41 +0300

    bugfix views and settings (#41)

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -197,9 +197,17 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorAndStaffOrReadOnly,)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get("review_id"),
+            title__id=self.kwargs.get("title_id")
+        )
         return review.comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get("review_id"),
+            title__id=self.kwargs.get("title_id")
+        )
         serializer.save(author=self.request.user, review=review)
